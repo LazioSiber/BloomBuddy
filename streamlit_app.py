43edf3e13ceb77480b3a47d42b9390264a372749
LazioSiber <149882809+LazioSiber@users.noreply.github.com>
2023-12-03 19:21:48 +0700

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,23 @@
 import streamlit as st
 import openai
 # Uncomment the following lines to enable the API key input form
+# Initialize
+st.cache_data.clear()
+
+if "openai_api_key" not in st.session_state:
+    st.session_state.openai_api_key = ""
+
+openai.api_key = st.session_state.openai_api_key
+
+if "text_error" not in st.session_state:
+    st.session_state.text_error = None
+
+if "text" not in st.session_state:
+    st.session_state.text = None
+
+if "n_requests" not in st.session_state:
+    st.session_state.n_requests = 0
+    
 with st.sidebar:
     api_key_form = st.form(key="api_key_form")
     openai_api_key = api_key_form.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
@@ -39,7 +56,6 @@ st.markdown("<h2 style = 'font-size: 1.8rem'>🌼Flower For Your Important Perso
 
 # Uncomment the following lines to enable the API key input form
 
-st.set_page_config(page_title="BloomBuddy", page_icon="🌼")
 
 # User input
 occasion = st.text_input("Occasion:")
