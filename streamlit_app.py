c77c8ba9112347b8539dd9d6cc2e4b2a2a1828aa
LazioSiber <149882809+LazioSiber@users.noreply.github.com>
2023-12-03 16:11:22 +0700

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,74 +1,19 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 9,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "def fact(n):\n",
-    "    result = 1\n",
-    "    for i in range (n):\n",
-    "        result = result * i+1\n",
-    "    print(result)\n",
-    "    return result"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 6,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "def main():\n",
-    "    fact(3)"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 10,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "def fact(n):\n",
-    "    result=1\n",
-    "    for i in range(n):\n",
-    "        result=result*i+1\n",
-    "    print (result)\n",
-    "    return 0"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 13,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "def main():\n",
-    "    fact(3)"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "base",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.11.4"
-  },
-  "orig_nbformat": 4
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+import openai
+import streamlit as st
+
+# Get the API key from the sidebar called OpenAI API key
+user_api_key = st.sidebar.text_input("OpenAI API key", type="password")
+
+client = openai.OpenAI(api_key=user_api_key)
+prompt = st.text_area("Tell me about your important person", height=200)
+openai.chat.completions.create(
+                model="gpt-3.5-turbo",
+                temperature=0.6,
+                top_p = 0.6,
+                max_tokens = 450,
+                messages=[
+                    {"role": "system", "content": f"You are a grammar checker bot that outputs in formatted html. Check the following text for grammar mistakes in the context of  Correct the errors and provide a list of (Fairly simple) explanations."},
+                    {"role": "user", "content": f"Please fix this, highlighting any changes in green and, more importantly, showing the original with mistakes in red: {prompt}"},
+                    ]
+                )
+            
