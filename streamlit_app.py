2ad04b3e427bf3f2c8a8b846a99f6d0c9016c4c6
LazioSiber <149882809+LazioSiber@users.noreply.github.com>
2023-12-05 17:19:07 +0700

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -124,6 +124,6 @@ if st.button("Generate Recommendation"):
         # Display the tables
         for index, row in df.iterrows():
             st.markdown(f"<h3>{row['Flower']}</h3>", unsafe_allow_html=True)
-            st.table(pd.DataFrame({'Notes': [row['Notes']]}).set_index('Notes', drop=True))
+            st.write(row['Notes'])
     else:
         st.warning("Please fill in all fields.")
